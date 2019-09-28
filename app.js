167c21e76c7fe3b8b1144e1a270f1bf670a810f4
Park Chanyoung <rhdwkrxla68@gmail.com>
2019-09-28 12:27:57 +0900

    타임라인 조절, 레이어 조정 기능 제작

--- app.js
+++ app.js
@@ -24,12 +24,27 @@
             if (!video) return;
             flag ? target.play() : target.pause();
         },
+        setTime(time) {
+            var video = document.getElementById('player');
+            video.currentTime = time;
+        },
+        visibleRects(currentTime, rects) {
+            var rects = rects.filter((obj) => obj.time.start <= currentTime && obj.time.end >= currentTime);
+            RectManager.init(rects);
+        },
+        getTimeByPosition(video, left, width) {
+            var duration = video.duration;
+            var part = duration / 100;
+            var start = part * left;
+            var end = part * width + start;
+            return { start, end };
+        },
     };
 
     var RectManager = {
         init(rects) {
+            rects = this.sortByFloor(rects || []);
             var target = document.getElementById('drawCanvas');
-
             this.clear(target);
             for (var rect of rects) {
                 var type = rect.type;
@@ -40,7 +55,6 @@
                         var startY = coords.startY;
                         var endX = coords.endX;
                         var endY = coords.endY;
-                        console.log(startX, startY);
                         var color = rect.color || 'yellow';
                         var canvas = document.getElementById('drawCanvas');
                         this.square(canvas, color, startX, startY, endX, endY);
@@ -61,6 +75,29 @@
             var ctx = canvas.getContext('2d');
             ctx.clearRect(0, 0, width, height);
         },
+        changeTime(rects, index, start, end) {
+            index *= 1;
+            var target = rects.filter((obj) => obj.index === index)[0];
+            var time = target.time
+            time.start = start;
+            time.end = end;
+        },
+        getMaxIndex(rects) {
+            var max = -1;
+            for (var rect of rects) {
+                max = max < rect.index ? rect.index : max;
+            }
+            return max;
+        },
+        push(rects, options) {
+            var index = this.getMaxIndex(rects) + 1;
+            options.index = index;
+            options.floor = rects.length + 1;
+            rects.push(options);
+        },
+        sortByFloor(rects) {
+            return (rects || []).sort((a, b) => a.floor > b.floor ? 1 : a.floor < b.floor ? -1 : 0);
+        },
     };
 
     var CoordsManager = {
@@ -76,11 +113,97 @@
         },
     };
 
+    var LayerManager = {
+        init(rects) {
+            this.clear();
+            var items = RectManager.sortByFloor(rects || []);
+            var player = document.getElementById('player');
+            var container = $('.layer');
+            for (var rect of items) {
+                var time = rect.time || {};
+                var startTime = time.start || 0;
+                var endTime = time.end || 20;
+                var left = (startTime / player.duration * 100);
+                var width = (endTime / player.duration * 100) - left;
+                var index = rect.index || 0;
+                this.createLayerProgress(index, left, width);
+            }
+            var layerProgresses = $('.layer__progress');
+            container.sortable({
+                containment: '.layer',
+                update(e) {
+                    var items = $('.layer__row');
+                    var player = document.getElementById('player');
+                    var currentTime = player.currentTime || 0;
+                    items.each(function() {
+                        var idx = $(this).data('idx');
+                        var floor = items.length - $(this).index();
+                        var rect = rects[idx];
+                        rect.floor = floor;
+                    });
+                    VideoManager.visibleRects(currentTime, rects);
+                },
+            });
+            layerProgresses.resizable({
+                containment: '.layer__row',
+                handles: 'e',
+                minHeight: 32,
+                resize: function() {
+                    var item = $(this);
+                    var parent = item.parent();
+                    var width = item.width() / parent.width() * 100;
+                    var left = item.position().left / parent.width() * 100;
+                    var video = document.getElementById('player');
+                    var currentTime = video.currentTime || 0;
+                    var time = VideoManager.getTimeByPosition(video, left, width);
+                    var index = item.data('idx');
+                    RectManager.changeTime(rects, index, time.start, time.end);
+                    VideoManager.visibleRects(currentTime, rects);
+                },
+            });
+            layerProgresses.draggable({
+                containment: '.layer__row',
+                axis: 'x',
+                drag: function() {
+                    var item = $(this);
+                    var index = item.attr('data-idx');
+                    var left = item.position().left / item.parent().width() * 100;
+                    var width = item.width() / item.parent().width() * 100;
+                    var time = VideoManager.getTimeByPosition(player, left, width) || {};
+                    var currentTime = player.currentTime || 0;
+                    RectManager.changeTime(rects, index, time.start, time.end);
+                    VideoManager.visibleRects(currentTime, rects);
+                },
+            });
+        },
+        createLayerProgress(index, left, width) {
+            var container = $('.layer');
+            var layerRow = $('<div class="layer__row"></div>');
+            var layerProgress = $('<div class="layer__progress"></div>');
+            layerRow.append(layerProgress);
+            layerRow.attr('data-idx', index);
+            container.prepend(layerRow);
+            layerProgress.css('left', `${left}%`);
+            layerProgress.css('width', `${width}%`);
+            layerProgress.attr('data-idx', index);
+        },
+        clear() {
+            var container = $('.layer');
+            container.find('.layer__row').remove();
+        },
+    };
+
     var triggerCanvas = document.getElementById('triggerCanvas');
-    var videoPosters = document.querySelectorAll('img[data-video]');
+    var videoPosters = (document.querySelectorAll('img[data-video]') || []);
     var playButton = (document.getElementsByClassName('play-btn') || [])[0];
     var allClearButton = (document.getElementsByClassName('all-delete-btn') || [])[0];
     var pauseButton = (document.getElementsByClassName('pause-btn') || [])[0];
+    var video = document.getElementById('player');
+    var timeline = document.getElementsByClassName('layer__timeline')[0];
+    var ableTimelineMove = true;
+    var colorComboBox = document.getElementById('color');
+    var lineComboBox = document.getElementById('line');
+    var fontSizeComboBox = document.getElementById('fontSize');
 
     playButton.addEventListener('click', () => VideoManager.play(true));
 
@@ -110,6 +233,7 @@
     triggerCanvas.addEventListener('mouseup', (e) => {
         flag = false;
         coords.push([e.offsetX, e.offsetY]);
+        var player = document.getElementById('player');
         var triggerCanvas = document.getElementById('triggerCanvas');
         var firstCoords = CoordsManager.start(coords) || [];
         var lastCoords = CoordsManager.end(coords) || [];
@@ -118,10 +242,11 @@
         var endX = lastCoords[0] || 0;
         var endY = lastCoords[1] || 0;
         var type = currentOption.type || 'box';
+        var color = currentOption.color || 'gray';
         var border = currentOption.border || '3px';
         var textSize = currentOption.textSize || '16px';
+        var currentTime = player.currentTime;
         var temp;
-
         RectManager.clear(triggerCanvas);
         if (startX > endX) {
             temp = startX;
@@ -134,14 +259,48 @@
             endY = temp;
         }
         var data = { startX, startY, endX, endY };
-        rects.push({ type, border, textSize, coords: data });
-        RectManager.init(rects);
+        var uid = rects.length || 0; // 임시
+        var time = { start: currentTime, end: currentTime + 5 };
+        RectManager.push(rects, { type, border, textSize, coords: data, uid, time, color });
+        VideoManager.visibleRects(currentTime, rects);
+        LayerManager.init(rects);
     });
     allClearButton.addEventListener('click', () => {
         var canvas = document.getElementById('drawCanvas');
         RectManager.clear(canvas);
+        LayerManager.clear();
     });
     pauseButton.addEventListener('click', () => {
         VideoManager.play(false);
+    });
+    video.addEventListener('timeupdate', () => {
+        if (!ableTimelineMove) return;
+        var currentTime = video.currentTime;
+        var videoLength = video.duration;
+        var left = currentTime / videoLength * 100;
+        var timeline = document.getElementsByClassName('layer__timeline')[0];
+        VideoManager.visibleRects(currentTime, rects);
+        timeline.style.left = `${left}%`;
+    });
+    $(timeline).draggable({
+        containment: '.layer',
+        axios: 'x',
+        drag: function() {
+            var item = $(this);
+            var left = $(timeline).position().left;
+            var maxWidth = $('.layer').width();
+            var currentTime = video.duration / maxWidth * left;
+            var player = document.getElementById('player');
+            ableTimelineMove = false;
+            VideoManager.setTime(currentTime);
+            VideoManager.visibleRects(currentTime, rects);
+        },
+        stop() {
+            ableTimelineMove = true;
+        },
+    });
+    colorComboBox.addEventListener('change', () => {
+        var color = colorComboBox.value;
+        currentOption.color = color;
     });
 })()
